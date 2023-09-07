de18b995da83624f298bf726fc07b19b61bd5c38
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-09-07 15:48:16 +0530

    Image in next js

---
+++ pages/pets.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+import img from '../public/1.jpg'
+
+
+const pets = () => {
+    return (
+        <div>
+            <Image src={img} placeholder='blur' alt='pet' width='280' height='420' />
+            {
+                ['1', '2', '3', '4', '5'].map(path => {
+                    return (
+                        <div key={path}>
+                            <Image src={`/${path}.jpg`} alt="pet" width="280" height="420" />
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default pets
